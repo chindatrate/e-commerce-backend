1436b58db36750766b809b260f9b4d819a637ea9
chinda trate <chindatrate@me.com>
2022-06-05 11:57:54 -0400

    update models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: "product_tag",
+  unique: false,
+  onDelete: 'CASCADE',
+  foreignKey: "product_id"
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: "product_tag",
+  unique: false,
+  onDelete: 'CASCADE',
+  foreignKey: "tag_id"
+});
 
 module.exports = {
   Product,
